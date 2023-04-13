74ebde097710e6d8009ccd540138d5600b96e8d2
PC-imatge-2022-2023 <licenses@enti.cat>
2023-04-13 22:00:30 +0530

    de lokos desaparece las maquinitas xd

--- Assets/Scripts/ATM.cs
+++ Assets/Scripts/ATM.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ATM : MonoBehaviour
 {
     public GameObject atmScreen;
+
+    myContents inputActions;
+
+    public UnityEvent myAction;
+
+    private void Awake()
+    {
+        inputActions = new myContents();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -22,5 +32,27 @@ public class ATM : MonoBehaviour
             Debug.Log("Left the Space");
             LeanTween.scale(atmScreen, Vector3.zero, 2);
         }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (inputActions.Player.ActionKey.WasPerformedThisFrame())
+            {
+                Debug.Log("I pressed the button");
+                myAction.Invoke();  
+            }
+        }
+    }
+
+    public void OnEnable()
+    {
+        inputActions.Player.Enable();
+    }
+
+    public void OnDisable()
+    {
+        inputActions.Player.Disable();
     }
 }
