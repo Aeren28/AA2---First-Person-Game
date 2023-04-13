3c01b671f2aa3424b053a5a3f26b0cda7adce039
PC-imatge-2022-2023 <licenses@enti.cat>
2023-04-13 23:05:32 +0530

    empezamos con los codigos wuaaajajaja

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -11,6 +11,9 @@ public class PuzzleManager : MonoBehaviour
     public GameObject[] PuzzlePiece4;
 
     private int purpleCounter;
+    private int cyanCounter; 
+    private int pinkCounter; 
+    private int coralCounter; 
 
     public void PurpleATM()
     {
@@ -48,6 +51,132 @@ public class PuzzleManager : MonoBehaviour
                 PuzzlePiece1[2].SetActive(false);
                 PuzzlePiece1[3].SetActive(false);
                 purpleCounter = 0;
+                break;
+
+            default:
+                break;
+        }
+    }
+    public void CyanATM()
+    {
+        cyanCounter++;
+
+        switch (cyanCounter)
+        {
+            case 0:
+                PuzzlePiece2[0].SetActive(true);
+                PuzzlePiece2[1].SetActive(false);
+                PuzzlePiece2[2].SetActive(false);
+                PuzzlePiece2[3].SetActive(false);
+                break;
+            case 1:
+                PuzzlePiece2[0].SetActive(false);
+                PuzzlePiece2[1].SetActive(true);
+                PuzzlePiece2[2].SetActive(false);
+                PuzzlePiece2[3].SetActive(false);
+                break;
+            case 2:
+                PuzzlePiece2[0].SetActive(false);
+                PuzzlePiece2[1].SetActive(false);
+                PuzzlePiece2[2].SetActive(true);
+                PuzzlePiece2[3].SetActive(false);
+                break;
+            case 3:
+                PuzzlePiece2[0].SetActive(false);
+                PuzzlePiece2[1].SetActive(false);
+                PuzzlePiece2[2].SetActive(false);
+                PuzzlePiece2[3].SetActive(true);
+                break;
+            case 4:
+                PuzzlePiece2[0].SetActive(true);
+                PuzzlePiece2[1].SetActive(false);
+                PuzzlePiece2[2].SetActive(false);
+                PuzzlePiece2[3].SetActive(false);
+                cyanCounter = 0;
+                break;
+
+            default:
+                break;
+        }
+    }
+    public void PinkATM()
+    {
+        pinkCounter++;
+
+        switch (pinkCounter)
+        {
+            case 0:
+                PuzzlePiece3[0].SetActive(true);
+                PuzzlePiece3[1].SetActive(false);
+                PuzzlePiece3[2].SetActive(false);
+                PuzzlePiece3[3].SetActive(false);
+                break;
+            case 1:
+                PuzzlePiece3[0].SetActive(false);
+                PuzzlePiece3[1].SetActive(true);
+                PuzzlePiece3[2].SetActive(false);
+                PuzzlePiece3[3].SetActive(false);
+                break;
+            case 2:
+                PuzzlePiece3[0].SetActive(false);
+                PuzzlePiece3[1].SetActive(false);
+                PuzzlePiece3[2].SetActive(true);
+                PuzzlePiece3[3].SetActive(false);
+                break;
+            case 3:
+                PuzzlePiece3[0].SetActive(false);
+                PuzzlePiece3[1].SetActive(false);
+                PuzzlePiece3[2].SetActive(false);
+                PuzzlePiece3[3].SetActive(true);
+                break;
+            case 4:
+                PuzzlePiece3[0].SetActive(true);
+                PuzzlePiece3[1].SetActive(false);
+                PuzzlePiece3[2].SetActive(false);
+                PuzzlePiece3[3].SetActive(false);
+                pinkCounter = 0;
+                break;
+
+            default:
+                break;
+        }
+    }
+    public void CoralATM()
+    {
+        coralCounter++;
+
+        switch (coralCounter)
+        {
+            case 0:
+                PuzzlePiece4[0].SetActive(true);
+                PuzzlePiece4[1].SetActive(false);
+                PuzzlePiece4[2].SetActive(false);
+                PuzzlePiece4[3].SetActive(false);
+                break;
+            case 1:
+                PuzzlePiece4[0].SetActive(false);
+                PuzzlePiece4[1].SetActive(true);
+                PuzzlePiece4[2].SetActive(false);
+                PuzzlePiece4[3].SetActive(false);
+                break;
+            case 2:
+                PuzzlePiece4[0].SetActive(false);
+                PuzzlePiece4[1].SetActive(false);
+                PuzzlePiece4[2].SetActive(true);
+                PuzzlePiece4[3].SetActive(false);
+                break;
+            case 3:
+                PuzzlePiece4[0].SetActive(false);
+                PuzzlePiece4[1].SetActive(false);
+                PuzzlePiece4[2].SetActive(false);
+                PuzzlePiece4[3].SetActive(true);
+                break;
+            case 4:
+                PuzzlePiece4[0].SetActive(true);
+                PuzzlePiece4[1].SetActive(false);
+                PuzzlePiece4[2].SetActive(false);
+                PuzzlePiece4[3].SetActive(false);
+                coralCounter = 0;
                 break;
 
             default:
